96d7dad556fe628de21a7745e187136c8eb5b9dd
FLORIDAMAN349 <beachhartonbos@gmail.com>
2024-10-28 07:56:10 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
-
+function getComputerPick(){ 
+  
+    //Here it makes a variable where it makes a random number between 1 and 3
+    var computerChoice = Randomizer.nextInt(1,3)
+    
+    
+    if (computerChoice == 1) {
+        return "rock";
+    } else if (computerChoice == 2) {
+        return "paper";
+    } else if (computerChoice == 3) {
+        return "scissors";
+    } else {
+        println("something is wrong please try again later")
+    }
+}
